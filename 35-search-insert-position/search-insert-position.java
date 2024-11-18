42deb72c0159a6aceb652bb3eba1d58882f9fb8e
Tiago Andre Batista Rodrigues <79528246+TiagoRodrigues-11@users.noreply.github.com>
2024-11-18 21:53:50 +0000

    Time: 0 ms (100.00%) | Memory: 42.7 MB (82.54%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -4,7 +4,7 @@ class Solution {
         int left = 0;
         int right = nums.length-1;
 
-        while (left < right) {
+        while (left <= right) {
             int mid = left + (right - left) / 2;
 
             if (nums[mid] == target) {
@@ -16,11 +16,7 @@ class Solution {
             }
 
         }
-
-        if (nums[left] < target) {
-            return left+1;
-        }
-
+        
         return left;
     }
 }
