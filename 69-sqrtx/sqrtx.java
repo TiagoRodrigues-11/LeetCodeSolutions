f9fe0ac2f66e43f14a7c66ef1698269145e13dcc
Tiago Andre Batista Rodrigues <79528246+TiagoRodrigues-11@users.noreply.github.com>
2025-01-09 15:03:21 +0000

    Time: 1 ms (87.08%) | Memory: 41.4 MB (5.47%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,29 +1,23 @@
 class Solution {
     public int mySqrt(int x) {
-        if (x == 0 || x == 1){
-            return x;
-        }
-        long left = 1;
-        long right = x;
+        long left = 1, right = x;
+        long res = 0;
 
-        while (right - left > 1) {
-            long mid = left + (right - left) / 2;
+        while (left <= right) {
+            long mid = (left + right) / 2;
             long squaredMid = mid * mid;
-            System.out.println(left + " - " + mid + " - " + right + ": " + squaredMid);
+
             if (squaredMid == x) {
                 return (int)mid;
             } else if (squaredMid > x) {
                 right = mid-1;
             } else {
-                left = mid;
+                res = mid;
+                left = mid+1;
             }
 
         }
-        if (right*right < x) {
-            return (int) right;
-        }
-        System.out.println(left + " - " + right);
 
-        return (int)left;
+        return (int) res;
     }
 }
