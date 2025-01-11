ba517d0625518a7503aefa549c825b3a467ea001
Tiago Andre Batista Rodrigues <79528246+TiagoRodrigues-11@users.noreply.github.com>
2025-01-11 15:41:53 +0000

    Time: 5 ms (72.00%) | Memory: 44.9 MB (5.11%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,9 +1,5 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0) {
-            return false;
-        }
-
         int tempX = x;
         int palindrome = 0;
         
