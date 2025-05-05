3e4a8388fa97486438f2c5ca1c28375241a8fcfd
Tiago Andre Batista Rodrigues <79528246+TiagoRodrigues-11@users.noreply.github.com>
2025-05-05 14:03:19 +0100

    Time: 218 ms (82.52%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,9 +1,7 @@
 -- Write your PostgreSQL query statement below
 
-
-SELECT id
-FROM (
-    SELECT id, temperature, LAG(temperature, 1) OVER (ORDER BY recordDate) AS previous_temperature, recordDate AS today, LAG(recordDate, 1) OVER (ORDER BY recordDate) AS yesterday
-    FROM Weather
-)
-WHERE today - yesterday = 1 AND temperature > previous_temperature;
+SELECT a.id
+FROM Weather a
+JOIN Weather b 
+ON a.recordDate = b.recordDate + INTERVAL '1 day'
+WHERE a.temperature > b.temperature;
